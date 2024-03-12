c36c1b64e52a7dac31d28a2771e692a237387f6a
Mrkhono <akhono@yahoo.com>
2024-03-12 11:50:55 +0000

    choix des images terminés

--- hubhtml.liquid-themes.com/assets/js/main.js
+++ hubhtml.liquid-themes.com/assets/js/main.js
@@ -17,26 +17,8 @@ document.getElementById("detecter").addEventListener('click', function(){
 });
 
 
-var verifyCallback = function(response) {
-    alert(response);
-  };
-  var widgetId1;
-  var widgetId2;
-  var onloadCallback = function() {
-    // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
-    // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-    widgetId1 = grecaptcha.render('example1', {
-      'sitekey' : '6Lf584opAAAAAG21dwO_S0Dn7BZBtFDZI5-c7E13',
-      'theme' : 'light'
-    });
-    widgetId2 = grecaptcha.render(document.getElementById('example2'), {
-      'sitekey' : '6Lf584opAAAAAG21dwO_S0Dn7BZBtFDZI5-c7E13'
-    });
-    grecaptcha.render('example3', {
-      'sitekey' : '6Lf584opAAAAAG21dwO_S0Dn7BZBtFDZI5-c7E13',
-      'callback' : verifyCallback,
-      'theme' : 'dark'
-    });
+function onSubmit(token) {
+  document.getElementById("demo-form").submit();
 }
 
 
